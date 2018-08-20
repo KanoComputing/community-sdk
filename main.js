ad894da4ac2b828146f2f2e4c367c89a234e4f74
Murilo Polese <murilopolese@gmail.com>
2018-08-20 20:29:21 +0100

    Create `main.js`

---
+++ main.js
@@ -0,0 +1,7 @@
+const DeviceManager = require('./src/manager');
+const MotionSensor = require('./src/motionsensorkit');
+
+module.exports = {
+    DeviceManager: DeviceManager,
+    MotionSensor: MotionSensor
+}
