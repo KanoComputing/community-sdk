d0e5ad002dd776998be5ca2951703dedd2881640
Murilo Polese <murilopolese@gmail.com>
2018-08-23 19:28:14 +0100

    Include `RetailPixelKitSerial` on `communitysdk` module

--- communitysdk/__init__.py
+++ communitysdk/__init__.py
@@ -1,6 +1,9 @@
 from .rpcclient import RPCClient
+from .serialdevice import SerialDevice
 from .devicemanager import list_connected_devices
 from .motionsensorkit import MotionSensorKit
+from .retailpixelkit import RetailPixelKitSerial
 
 
-__all__ = ['RPCClient', 'MotionSensorKit', 'list_connected_devices']
+__all__ = ['RPCClient', 'SerialDevice', 'MotionSensorKit',
+    'RetailPixelKitSerial', 'list_connected_devices']
