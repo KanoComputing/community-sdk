41a890a3b09a71e57df902287dfd75623c2bf887
Murilo Polese <murilopolese@gmail.com>
2018-08-23 01:07:15 +0100

    Configure `__init__.py` for `communitysdk`

--- communitysdk/__init__.py
+++ communitysdk/__init__.py
@@ -1,4 +1,6 @@
 from .rpcclient import RPCClient
+from .devicemanager import list_connected_devices
+from .motionsensorkit import MotionSensorKit
 
 
-__all__ = ['RPCClient',]
+__all__ = ['RPCClient', 'MotionSensorKit', 'list_connected_devices']
